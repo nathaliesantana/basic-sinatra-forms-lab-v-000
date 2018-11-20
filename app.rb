413502328756290d35cc5a8c8018ac5c11880c74
Nathalie Santana <nathalie.scruz@gmail.com>
2018-11-20 02:48:42 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,12 +10,12 @@ class App < Sinatra::Base
   
   post '/team' do
     @name= params[:name]
-    @coach= params[:]
-    @pg= params[:]
-    @sg= params[:]
-    @pf= params[:]
-    @sf= params[:]
-    @c= params[:] 
+    @coach= params[:coach]
+    @pg= params[:pg]
+    @sg= params[:sg]
+    @pf= params[:pf]
+    @sf= params[:sf]
+    @c= params[:c] 
     
     erb :team
   end
